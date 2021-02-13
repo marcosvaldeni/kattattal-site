482ac9ca0df1da104a33086ce46ea211269b689d
Marcos Lucas <marcosvaldeni@gmail.com>
2021-02-13 13:04:22 +0000

    navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa';
+
+import { 
+  Nav, 
+  NavbarContainer, 
+  NavLogo,
+  MobileIcon,
+  NavItem,
+  NavLinks,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './styles';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+
+          <NavLogo to="/">Kattattal</NavLogo>
+
+
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to="play">What I Play</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to="channel">Channels</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to="podcast">Podcast</NavLinks>
+            </NavItem>
+
+          </NavMenu>
+
+          <NavBtn>
+              <NavBtnLink to="signin">Sign In</NavBtnLink>
+            </NavBtn>
+
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar;
---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Exo 2', sans-serif;
+  }
+`;
